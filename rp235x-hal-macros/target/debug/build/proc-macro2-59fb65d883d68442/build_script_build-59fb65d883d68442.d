c31457c1529bfae3756e9ec8e4546506f0ee4d71
Ramenspazz <kirisame.ramen@gmail.com>
2026-04-17 14:25:40 +0000

    Delete rp235x-hal-macros directory

--- rp235x-hal-macros/target/debug/build/proc-macro2-59fb65d883d68442/build_script_build-59fb65d883d68442.d
+++
@@ -1,5 +0,0 @@
-/home/ramenspazz/Programs/rp-hal/rp235x-hal-macros/target/debug/build/proc-macro2-59fb65d883d68442/build_script_build-59fb65d883d68442.d: /home/ramenspazz/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.106/build.rs
-
-/home/ramenspazz/Programs/rp-hal/rp235x-hal-macros/target/debug/build/proc-macro2-59fb65d883d68442/build_script_build-59fb65d883d68442: /home/ramenspazz/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.106/build.rs
-
-/home/ramenspazz/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.106/build.rs:
